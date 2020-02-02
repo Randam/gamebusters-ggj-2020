a0795f1ba0dd2f28868f98ef28c2f8d4f7c2cfb7
Randam <jeroen@derwort.nl>
2020-02-02 14:03:09 +0100

    added assets & title

--- src/game/game.ts
+++ src/game/game.ts
@@ -9,7 +9,7 @@ const config: GameConfig = {
     type: Phaser.AUTO,
     width: 1360,
     height: 750,
-    scene: [MainScene, BootScene, GameOverScene],
+    scene: [GameOverScene, MainScene, BootScene],
     backgroundColor: 0x444444,
 
     // physics settings
